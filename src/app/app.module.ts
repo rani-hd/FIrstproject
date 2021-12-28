f8815c68606d5576b14ff9272ad36de81a150b6d
Angular <Rani@hudsondata.com>
2021-12-28 11:43:31 -0700

    project updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatTableModule} from '@angular/material/table';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from './search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
-
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
+   
     SearchComponent,
     LoginComponent
     ],
