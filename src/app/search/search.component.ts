55b712284dc22d6e301485905e9ed2070590ef67
Angular <Rani@hudsondata.com>
2022-01-03 02:05:05 -0700

    message

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,67 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import {CountryDataService} from '../services/country-data.service'
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+import { CountryDataService } from '../services/country-data.service';
+
+export interface countryData {
+  country_name: string;
+  univercity_name: string;
+  stateProvince: string;
+  id: string;
+  domain: string;
 }
-
-const userdata: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
-  name:string="";
-  cityName:string=""
-  countryName:string=""
-  allCountries = ['India','Pakistan','USA','Netherland','Swizerland','Brazil','Africa']
+export class SearchComponent implements OnInit, AfterViewInit {
+  name: string = '';
+  cityName: string = '';
+  countryName: string = '';
 
-  country:any;
-  constructor( private router:Router,private countryData:CountryDataService) { 
-    
+  allCountries = [
+    'India',
+    'Pakistan',
+    'USA',
+    'Netherland',
+    'Swizerland',
+    'Brazil',
+    'Africa',
+  ];
+  country: any;
+
+  displayedColumns: string[] = [
+    'id',
+    'univercity_name',
+    'stateProvince',
+    'country_name',
+    'domain',
+  ];
+  DATA: any[] = [];
+  dataAvailabel: boolean = false;
+  dataSource: MatTableDataSource<countryData>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private router: Router,
+    private countryData: CountryDataService
+  ) {}
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-  users= userdata;
-   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource =this.users;
-
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
   ngOnInit(): void {
-   const storedName =localStorage.getItem('name')
-   if(storedName){
-     this.name = JSON.parse(storedName)
-   }
+    const storedName = localStorage.getItem('name');
+    if (storedName) {
+      this.name = JSON.parse(storedName);
+    }
   }
-  logoutUser():any{
-    localStorage.removeItem('name')
-    this.router.navigate(["login"])
-  }
-
-  search():any{
-    console.log('you are searching for',this.countryName,this.cityName)
-    this.countryData.getData(this.countryName,this.cityName).subscribe((data)=>{
-      console.log("data",data);
-      let counter=localStorage.getItem('counter')||'0';
-      let value=JSON.parse(counter)
-      value = value +1;
-      localStorage.setItem('counter',JSON.stringify(value));
-    })
+  logoutUser(): any {
+    localStorage.removeItem('name');
+    this.router.navigate(['login']);
   }
 
+  search(): any {
+    console.log('you are searching for', this.countryName, this.cityName);
+    this.countryData
+      .getData(this.countryName, this.cityName)
+      .subscribe((data: any) => {
+        this.DATA = data.map((item: any, i: number) => {
+          return {
+            country_name: item.country,
+            univercity_name: item.name,
+            stateProvince: item['state-province'] || '-',
+            id: (i + 1).toString(),
+            domain: item.web_pages[0],
+          };
+        });
+        this.dataSource = new MatTableDataSource(this.DATA);
+        this.dataAvailabel = true;
+
+        let counter = localStorage.getItem('counter') || '0';
+        let value = JSON.parse(counter);
+        value = value + 1;
+        localStorage.setItem('counter', JSON.stringify(value));
+      });
+  }
 }
