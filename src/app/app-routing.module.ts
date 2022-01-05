8181bc4a5c025f6829a56f3e24e2680b932545c8
Angular <Rani@hudsondata.com>
2022-01-05 09:19:26 -0700

    code updated-002

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './authGuard/auth.guard';
+
 const routes: Routes = [
-{path:'search',component:SearchComponent},
+{path:'search',component:SearchComponent,canActivate: [AuthGuard]},
 {path:'login',component:LoginComponent},
 {path:"",redirectTo:'/login',pathMatch:"full"}];
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,20 +9,22 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   username: string = '';
   counter: Number = 0;
-  constructor(private router: Router) {}
+  constructor(private router: Router,private authService: AuthService) {}
   ngOnInit(): void {
     // this.username = "Rani"
   }
 
   addUser(): any {
-    if(this.username==""){
-      alert("Please enter name")
-    }    
-    else{
-    console.log('My name is', this.username);
-    localStorage.setItem('name', JSON.stringify(this.username.toLowerCase()));
-    localStorage.setItem('counter', JSON.stringify(this.counter));
-    this.router.navigate(['search']);
+    if (this.username == '') {
+      alert('Please enter name');
+    } else {
+      console.log('My name is', this.username);
+      localStorage.setItem('name', JSON.stringify(this.username.toLowerCase()));
+      localStorage.setItem('counter', JSON.stringify(this.counter));
+      const authValue = this.authService.login(this.username);
+      if(authValue == true){
+        this.router.navigate(['search']);
+      }
     }
   }
 
