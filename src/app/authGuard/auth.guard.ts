5ab1ef23ff1e5b7c59e34c24c26d2202cd563abb
Angular <Rani@hudsondata.com>
2022-01-05 09:11:23 -0700

    code updated

--- src/app/authGuard/auth.guard.ts
+++ src/app/authGuard/auth.guard.ts
@@ -9,15 +9,14 @@ import { AuthService } from '../auth/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService,private router: Router) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.authService.isAuthenticate) {
-        return true;
-      } else {
-        this.router.navigate(['login']);
-        return false;
-      }
+  canActivate():boolean{
+    if(this.authService.isAuthenticated()){
+      return true;
+    }else{
+      this.router.navigate(['login']);
+      return false;
+    }
   }
+
   
 }
